95c2f7062fbaea6973be2ee6a14b76a857420d35
Pawel Urbanik <pawelurbanik94@gmail.com>
2020-04-20 18:56:50 +0200

    first commit

---
+++ src/main/java/pl/pawel/weatherapp/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package pl.pawel.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppApplication.class, args);
+    }
+
+}
