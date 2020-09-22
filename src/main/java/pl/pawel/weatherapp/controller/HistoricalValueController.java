9bb2e3aa1bb529e7cb056b200166fdfdd0d92a58
Pawel <pawelurbanik94@gmail.com>
2020-09-22 19:49:03 +0200

    Checkbox form in with possible ciities on historical temperature page

--- src/main/java/pl/pawel/weatherapp/controller/HistoricalValueController.java
+++ src/main/java/pl/pawel/weatherapp/controller/HistoricalValueController.java
@@ -2,14 +2,20 @@ package pl.pawel.weatherapp.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.pawel.weatherapp.model.response.CurrentWeatherResponse;
 import pl.pawel.weatherapp.repository.CurrentWeatherRepository;
 import pl.pawel.weatherapp.service.ExcelFileExporter;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/historic")
@@ -23,10 +29,14 @@ public class HistoricalValueController {
 
     }
 
-    @GetMapping("/save")
-    public void exportToExcel(HttpServletResponse response) throws IOException {
+    @PostMapping("/save")
+    public void exportToExcel(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        System.out.println(Arrays.toString(request.getParameterValues("cities")));
 
         final String filename = "weather_new.xlsx";
+        //TODO zmiana z findAll na find by name
         List<CurrentWeatherResponse> currentWeatherResponses = currentWeatherRepository.findAll();
         response.setContentType("application/octet-stream");
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
@@ -42,7 +52,9 @@ public class HistoricalValueController {
 
 
     @GetMapping()
-    public String historicalValue(){
+    public String historicalValue(Model model){
+        Set<String> cities = currentWeatherRepository.findAllCities();
+        model.addAttribute("cities", cities);
         return "save";
     }
 }
--- src/main/java/pl/pawel/weatherapp/repository/CurrentWeatherRepository.java
+++ src/main/java/pl/pawel/weatherapp/repository/CurrentWeatherRepository.java
@@ -1,13 +1,20 @@
 package pl.pawel.weatherapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.pawel.weatherapp.model.response.CurrentWeatherResponse;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface CurrentWeatherRepository extends JpaRepository<CurrentWeatherResponse, Long> {
 
     Optional<CurrentWeatherResponse> findCurrentWeatherResponseByName(String name);
+
+    @Query(nativeQuery = true, value =
+            "select cwr.name as name from CURRENT_WEATHER_RESPONSE cwr")
+    Set<String> findAllCities();
 }
